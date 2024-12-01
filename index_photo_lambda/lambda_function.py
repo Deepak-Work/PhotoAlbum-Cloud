1b6d342f77eb1dc9d65e5e421bba6100ac19d49a
Deepak-Work <deepakchaurasia.work@gmail.com>
2024-11-30 20:15:35 -0500

    Adding code for the lambdas

---
+++ index_photo_lambda/lambda_function.py
@@ -0,0 +1,112 @@
+import boto3
+import logging
+from botocore.exceptions import ClientError
+import json
+import base64
+from elasticsearch import Elasticsearch
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import os
+
+# Instantiate logger
+logger = logging.getLogger(__name__)
+
+# connect to the Rekognition client
+rekognition = boto3.client('rekognition')
+s3 = boto3.client('s3')
+# es_endpoint = os.getenv('elasticsearch_endpoint')
+
+# # Initialize Elasticsearch client
+# es = Elasticsearch([{'host': es_endpoint, 'port': 443, 'use_ssl': True}])
+
+
+# Set up OpenSearch client
+OPENSEARCH_HOST = os.environ['OPENSEARCH_HOST']
+OPENSEARCH_USERNAME = os.environ['OPENSEARCH_USERNAME']
+OPENSEARCH_PASSWORD = os.environ['OPENSEARCH_PASSWORD']
+
+os_client = OpenSearch(
+    hosts=[OPENSEARCH_HOST],
+    http_auth=(OPENSEARCH_USERNAME, OPENSEARCH_PASSWORD),  # Add your OpenSearch credentials if necessary
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+
+def lambda_handler(event, context):
+
+    event = event['Records'][0]
+    bucket = event['s3']['bucket']['name']
+    key = event['s3']['object']['key']
+
+
+    try:
+        image = None
+        s3_object = s3.head_object(Bucket = bucket, Key = key)
+        custom_labels = s3_object.get('Metadata', {}).get('x-amz-meta-customlabels', '')
+        
+        A1 = [label.strip() for label in custom_labels.split(',')] if custom_labels else []
+
+        image = s3.get_object(Bucket = bucket, Key = key)['Body'].read()
+
+        response = rekognition.detect_labels(Image={'Bytes': image})
+
+        labels = A1 + [label['Name'] for label in response['Labels']]
+
+        es_document = {
+            "objectKey": key,
+            "bucket": bucket,
+            "createdTimestamp": s3_object['LastModified'].isoformat(),
+            "labels": labels
+        }
+
+        print("JSON object for ElasticSearch:")
+        print(json.dumps(es_document, indent=2))
+        statusCode = None
+
+        try:
+            os_client.index(index="photos", body=es_document)
+            # es.index(index="photos", body=es_document)
+        except Exception as e:
+            print("ERROR IN INDEXING")
+            print(e)
+            statusCode = 500
+
+        print(response)
+        lambda_response = {
+            "statusCode": statusCode if statusCode else 200,
+            "body": json.dumps(response)
+        }
+        print("Labels found:")
+        print(labels)
+
+    except ClientError as client_err:
+
+       error_message = "Couldn't analyze image: " + client_err.response['Error']['Message']
+
+       lambda_response = {
+           'statusCode': 400,
+           'body': {
+               "Error": client_err.response['Error']['Code'],
+               "ErrorMessage": error_message
+           }
+       }
+       logger.error("Error function %s: %s",
+                    context.invoked_function_arn, error_message)
+
+
+    except ValueError as val_error:
+
+        lambda_response = {
+            'statusCode': 400,
+            'body': {
+                "Error": "ValueError",
+                "ErrorMessage": format(val_error)
+            }
+        }
+        logger.error("Error function %s: %s",
+                     context.invoked_function_arn, format(val_error))
+
+    return lambda_response
+
---
+++ search_photo_lambda/lambda_function.py
@@ -0,0 +1,65 @@
+import boto3
+import json
+import os
+from opensearchpy import OpenSearch, RequestsHttpConnection
+
+lex = boto3.client('lexv2-runtime')
+
+OPENSEARCH_HOST = os.environ['OPENSEARCH_HOST']
+OPENSEARCH_USERNAME = os.environ['OPENSEARCH_USERNAME']
+OPENSEARCH_PASSWORD = os.environ['OPENSEARCH_PASSWORD']
+
+LEX_ALIAS_ID = os.environ['BOT_ALIAS_ID']
+LEX_BOT_ID = os.environ['BOT_ID']
+
+
+os_client = OpenSearch(
+    hosts=[OPENSEARCH_HOST],
+    http_auth=(OPENSEARCH_USERNAME, OPENSEARCH_PASSWORD),  # Add your OpenSearch credentials if necessary
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+def lambda_handler(event, context):
+    query = event['queryStringParameters']['q']
+    
+    # Disambiguate query using Lex
+    lex_response = lex.recognize_text(
+        botId = LEX_BOT_ID,
+        botAliasId = LEX_ALIAS_ID,
+        localeId='en_US',
+        sessionId='test-session',
+        text=query
+    )
+    
+    # Extract keywords from Lex response
+    keywords = []
+    if 'interpretations' in lex_response:
+        for interpretation in lex_response['interpretations']:
+            if 'intent' in interpretation:
+                for slot in interpretation['intent']['slots'].values():
+                    if slot and 'value' in slot:
+                        keywords.append(slot['value']['interpretedValue'])
+    
+    # If keywords found, search OpenSearch
+    if keywords:
+        search_query = {
+            "query": {
+                "multi_match": {
+                    "query": " ".join(keywords),
+                    "fields": ["labels"]
+                }
+            }
+        }
+        search_results = os_client.search(index="photos", body=search_query)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(search_results['hits']['hits'])
+        }
+    else:
+        # Return empty array if no keywords found
+        return {
+            'statusCode': 200,
+            'body': json.dumps([])
+        }
